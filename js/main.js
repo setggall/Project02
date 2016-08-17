8b7539bdb0851a550a8c432ecda9350116ebba78
Seth Gallimore <trips66@gmail.com>
2016-08-16 19:35:29 -0500

    Pagination buttons working now.

--- js/main.js
+++ js/main.js
@@ -10,28 +10,47 @@
 
 //Send list item to an array.
 var studentArray = $('.student-item').toArray();
-//Number of entries to show per page.
-var numberToShow = 10;
-//function to show 1st 10 list items and hide the rest.
-var pageLoad = function() {
-  $(studentArray).hide();
-  $('.student-item:nth-child(-n+10)').show();
-};
+var pagination = $('.page').append('<div class="pagination"><ul></ul></div>');
+var secondaryArray = [];
+//Get number of pages needed and append them into the empty div added to the page.
+function pages(array) {
+  var numberToShow = 10;
+  var numberOfPages = Math.ceil(studentArray.length / numberToShow);
+  for (var i = 0; i < numberOfPages; i++) {
+    $('.pagination ul').append('<li><a href="#">' + (i+1) + '</a></li>');
+  }
+  $('.pagination').find('a').first().addClass('active');
+}
 
-pageLoad();
+function entriesToShow (primary, currentArray, start, end) {
+  currentArray = primary.slice(start, end);
+  $(primary).hide();
+  $(currentArray).show();
+}
+
+function navClick(active) {
+  $('.pagination').find('a').removeClass('active');
+  $(active).addClass('active');
+}
+
+function pagClick(array, newArray) {
+  pagination.on('click', 'a', function(prev) {
+    prev.preventDefault();
+    var clicked = prev.target;
+    var filter = $(clicked).html();
+    var numberToShow = 10;
+    var beginSlice = (filter * numberToShow) - numberToShow;
+    var endSlice = beginSlice + numberToShow;
+    navClick(clicked);
+    entriesToShow(array, newArray, beginSlice, endSlice);
+  });
+}
+//Show the first 10 entries in Student Array on initial page load.
+$(studentArray).hide().slice(0, 10).show();
+
 //Add search field and button
 $('.page-header').append('<div class="student-search"><input placeholder="Enter search here."><button>Search</button></div>');
 //Add empty div to add page buttons to.
-$('.student-list').after('<div class="pagination"><ul></ul></div>');
-
-//create page buttons and get the correct number of pages.
-var pageButtons = function() {
-  $('.pagination ul li').remove();
-  var pageNumber = Math.ceil(studentArray.length / numberToShow);
-  for (var i = 0; i < pageNumber; i++) {
-    $('.pagination ul').append('<li><a href="#">' + (i+1) + '</a></li>');
-  }
-  $('.pagination ul li a').eq(0).addClass('active');
-};
-//call function to add the page buttons.
-pageButtons();
+$('.page').append(pagination);
+pages();
+pagClick(studentArray, secondaryArray)
